8668bbe317921563d8f667ad7773c14980aaed7a
Pierre-Emmanuel Lévesque <pierre.e.levesque@gmail.com>
2019-06-30 15:56:13 -0400

    cleaned up tests

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ const hasRequiredSubstringsAtIndexes = require('../index')
 
 describe('#hasRequiredSubstringsAtIndexes()', () => {
   describe('value check', () => {
-    it('should return true when required substrings is an empty object', () => {
+    it('should return true when requiredSubstrings is an empty object', () => {
       const str = ''
       const requiredSubstrings = {}
       const result = hasRequiredSubstringsAtIndexes(str, requiredSubstrings)
@@ -14,7 +14,7 @@ describe('#hasRequiredSubstringsAtIndexes()', () => {
       expect(result).to.equal(expected)
     })
 
-    it('should return false when str is empty and some required substrings are defined', () => {
+    it('should return false when str is empty, but requiredSubstrings is not empty', () => {
       const str = ''
       const requiredSubstrings = { 0: 'a' }
       const result = hasRequiredSubstringsAtIndexes(str, requiredSubstrings)
@@ -24,7 +24,7 @@ describe('#hasRequiredSubstringsAtIndexes()', () => {
   })
 
   describe('single character substrings', () => {
-    it('should return false when one required substring is not found', () => {
+    it('should return false when one of one required substring is not found', () => {
       const str = 'abcde'
       const requiredSubstrings = { 2: 'a' }
       const result = hasRequiredSubstringsAtIndexes(str, requiredSubstrings)
@@ -32,7 +32,7 @@ describe('#hasRequiredSubstringsAtIndexes()', () => {
       expect(result).to.equal(expected)
     })
 
-    it('should return true when one required substring is found', () => {
+    it('should return true when one of one required substring is found', () => {
       const str = 'abcde'
       const requiredSubstrings = { 2: 'c' }
       const result = hasRequiredSubstringsAtIndexes(str, requiredSubstrings)
@@ -40,7 +40,7 @@ describe('#hasRequiredSubstringsAtIndexes()', () => {
       expect(result).to.equal(expected)
     })
 
-    it('should return false when some substrings are not found', () => {
+    it('should return false when some of many substrings are not found', () => {
       const str = 'abcde'
       const requiredSubstrings = { 0: 'e', 2: 'c', 4: 'a' }
       const result = hasRequiredSubstringsAtIndexes(str, requiredSubstrings)
@@ -48,7 +48,7 @@ describe('#hasRequiredSubstringsAtIndexes()', () => {
       expect(result).to.equal(expected)
     })
 
-    it('should return true when many substrings are found', () => {
+    it('should return true when all of many substrings are found', () => {
       const str = 'abcde'
       const requiredSubstrings = { 0: 'a', 2: 'c', 4: 'e' }
       const result = hasRequiredSubstringsAtIndexes(str, requiredSubstrings)
@@ -58,7 +58,7 @@ describe('#hasRequiredSubstringsAtIndexes()', () => {
   })
 
   describe('multi character substrings', () => {
-    it('should return false when one required substring is not found', () => {
+    it('should return false when one of one required substring is not found', () => {
       const str = 'abcde'
       const requiredSubstrings = { 2: 'abc' }
       const result = hasRequiredSubstringsAtIndexes(str, requiredSubstrings)
@@ -66,7 +66,7 @@ describe('#hasRequiredSubstringsAtIndexes()', () => {
       expect(result).to.equal(expected)
     })
 
-    it('should return true when one required substring is found', () => {
+    it('should return true when one of one required substring is found', () => {
       const str = 'abcde'
       const requiredSubstrings = { 2: 'cde' }
       const result = hasRequiredSubstringsAtIndexes(str, requiredSubstrings)
@@ -74,7 +74,7 @@ describe('#hasRequiredSubstringsAtIndexes()', () => {
       expect(result).to.equal(expected)
     })
 
-    it('should return false when some substrings are not found', () => {
+    it('should return false when some of many substrings are not found', () => {
       const str = 'abcde'
       const requiredSubstrings = { 0: 'edc', 2: 'cde', 4: 'abc' }
       const result = hasRequiredSubstringsAtIndexes(str, requiredSubstrings)
@@ -82,7 +82,7 @@ describe('#hasRequiredSubstringsAtIndexes()', () => {
       expect(result).to.equal(expected)
     })
 
-    it('should return true when many substrings are found', () => {
+    it('should return true when all of many substrings are found', () => {
       const str = 'abcde'
       const requiredSubstrings = { 0: 'abc', 2: 'cde', 4: 'e' }
       const result = hasRequiredSubstringsAtIndexes(str, requiredSubstrings)
@@ -92,22 +92,48 @@ describe('#hasRequiredSubstringsAtIndexes()', () => {
   })
 
   describe('allowSubstringBleeding flag', () => {
-    it('should not allow bleeding when set to false', () => {
-      const str = 'abcde'
-      const requiredSubstrings = { 0: 'abc', 2: 'cde', 4: 'efg' }
-      const allowSubstringBleeding = false
-      const result = hasRequiredSubstringsAtIndexes(str, requiredSubstrings, allowSubstringBleeding)
-      const expected = false
-      expect(result).to.equal(expected)
+    describe('normal argument style', () => {
+      it('should not allow bleeding when set to false', () => {
+        const str = 'abcde'
+        const requiredSubstrings = { 0: 'abc', 2: 'cde', 4: 'efg' }
+        const allowSubstringBleeding = false
+        const result = hasRequiredSubstringsAtIndexes(str, requiredSubstrings, allowSubstringBleeding)
+        const expected = false
+        expect(result).to.equal(expected)
+      })
+
+      it('should allow bleeding when set to true', () => {
+        const str = 'abcde'
+        const requiredSubstrings = { 0: 'abc', 2: 'cde', 4: 'efg' }
+        const allowSubstringBleeding = true
+        const result = hasRequiredSubstringsAtIndexes(str, requiredSubstrings, allowSubstringBleeding)
+        const expected = true
+        expect(result).to.equal(expected)
+      })
     })
 
-    it('should allow bleeding when set to true', () => {
-      const str = 'abcde'
-      const requiredSubstrings = { 0: 'abc', 2: 'cde', 4: 'efg' }
-      const allowSubstringBleeding = true
-      const result = hasRequiredSubstringsAtIndexes(str, requiredSubstrings, allowSubstringBleeding)
-      const expected = true
-      expect(result).to.equal(expected)
+    describe('options style', () => {
+      it('should not allow bleeding when set to false', () => {
+        const str = 'abcde'
+        const requiredSubstrings = { 0: 'abc', 2: 'cde', 4: 'efg' }
+        const allowSubstringBleeding = false
+        const result = hasRequiredSubstringsAtIndexes(str, requiredSubstrings, {
+          allowSubstringBleeding: allowSubstringBleeding
+        })
+        const expected = false
+        expect(result).to.equal(expected)
+      })
+
+      it('should allow bleeding when set to true', () => {
+        const str = 'abcde'
+        const requiredSubstrings = { 0: 'abc', 2: 'cde', 4: 'efg' }
+        const allowSubstringBleeding = true
+        const result = hasRequiredSubstringsAtIndexes(str, requiredSubstrings, {
+          allowSubstringBleeding: allowSubstringBleeding
+        })
+        const expected = true
+        expect(result).to.equal(expected)
+      })
     })
   })
 })
